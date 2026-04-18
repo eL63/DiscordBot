cefb2263d174506acc6b1136cda778dbb028ed21
lui <luigi.danho99@gmail.com>
2026-04-18 20:54:01 +0200

    Discord Bot kann jetzt Nachrichten löschen

--- src/main/java/org/discord/MessageListener.java
+++ src/main/java/org/discord/MessageListener.java
@@ -7,12 +7,20 @@ public class MessageListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         if (event.getAuthor().isBot()) return;
-
+        String[] args = event.getMessage().getContentRaw().split("\\s+");
         if (event.getMessage().getContentRaw().equalsIgnoreCase("!hallo")) {
             event.getChannel().sendMessage("Hallo " + event.getAuthor().getName() + "!").queue();
         } else if (event.getMessage().getContentRaw().equalsIgnoreCase("!witz")) {
-            event.getChannel().sendMessage("So I was on discord and this guy tells me: \nI have an 11 kill streak. \n" +
-                    "So i asked on wich game? \nHe said, no I don't play games, I'm a police officer.").queue();
+            event.getChannel().sendMessage("Mein Bot ist so intelligent, er antwort auf Fragen, " +
+                    "die ich noch gar nicht gestellt habe.").queue();
+        } else if (event.getMessage().getContentRaw().equalsIgnoreCase("!wer")) {
+            event.getChannel().sendMessage("Wer gefragt hat?").queue();
+        } else if (args[0].equalsIgnoreCase("!löschen")) {
+            int amount = Integer.parseInt(args[1]);
+            event.getChannel().getIterableHistory()
+                    .takeAsync(amount)
+                    .thenAccept(messages -> event.getChannel().purgeMessages(messages));
+            
         }
     }
 }
